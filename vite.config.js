16bd7a6b93ae8f733e79b2e977ff7197240afb66
Jesmin1230 <tongmin@moh.gov.my>
2025-12-30 16:10:39 +0800

    Final path fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/pharmacy-schedule/', // <--- THIS LINE IS THE FIX
+  base: '/pharmacy-schedule/', 
 })
