31e7bd5da316c9017971c6bc5d2981b97b1f32e3
Jesmin1230 <tongmin@moh.gov.my>
2025-12-30 15:56:59 +0800

    Fixing white page error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Add this if you followed the v4 fix
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/pharmacy-schedule/', // <--- IMPORTANT: Use your repository name here
+  base: '/pharmacy-schedule/', // <--- THIS LINE IS CRUCIAL
 })
